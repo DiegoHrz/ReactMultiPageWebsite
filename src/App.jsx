a5735519930ca85bdb682d447a4f5a14374f5c8f
DiegoHrz <hernandezwdiego@gmail.com>
2023-08-23 23:48:59 -0500

    Add React routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
 import Navbar from './components/header/Navbar'
+import Home from './pages/1.home/Home'
+import Services from './pages/2.services/Services'
+import Portfolio from './pages/3.portfolio/Portfolio'
+import Contact from './pages/4.Contact/Contact'
 
 function App() {
 
 
   return (
-    <div className='w-screen h-screen bg-red-300'>
+    <div >
       <Navbar/>
+      <Home/>
+      <Services/>
+      <Portfolio/>
+      <Contact/>
     </div>
   )
 }
